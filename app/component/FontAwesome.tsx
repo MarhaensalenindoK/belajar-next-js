5e01f4128e4b4b899d5a91bd62bd9c496d25fa60
Marhaensalenindo Komara <marhaensa@gmail.com>
2024-04-21 22:43:55 +0700

    fix

--- app/component/FontAwesome.tsx
+++ app/component/FontAwesome.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { FontAwesomeIcon as FAIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon as FAIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css'; 
 
 library.add(fas);
 
-const FontAwesomeIcon = (props) => <FAIcon {...props} />;
+const FontAwesomeIcon = (props: React.JSX.IntrinsicAttributes & FontAwesomeIconProps) => <FAIcon {...props} />;
 export default FontAwesomeIcon;
